48bff32bc0f3fe2d719fd8dfa1529843d4f6c847
Jakub Krzywoń <jakubkrzywon@gmail.com>
2018-09-28 22:59:17 +0200

    commit

--- src/article/article.component.js
+++ src/article/article.component.js
@@ -57,10 +57,7 @@ class Article extends Component {
         console.log(this.state.data);
         const mov = this.state.data;
         console.log(mov);
-        // if (mov === undefined) {
-        //     this.props.history.push("/wiecej/" + this.props.match.params.category + "/" + (Number(this.props.match.params.id)+1))
-        //     return null
-        // }
+
 
         const list = <div key={mov.id} className="article-col-1-3">
             <img src={mov.image} className="article-col-img"/>
@@ -77,40 +74,7 @@ class Article extends Component {
             </div>
             <div className="header-container">{list}</div>
         </div>;
-
-    //     if (mov.id <= 29) {
-    //         return <div className="Article">
-    //             <div className="header-list">
-    //                 <Link to={"/10"}>Artykuły</Link>
-    //                 <Link to="/">Strona główna</Link>
-    //                 <Link to={"/wiecej/10" + "/" + (Number(mov.id) + 1)}>Następny
-    //                     artykuł &rarr;</Link>
-    //             </div>
-    //             <div className="header-container">{list}</div>
-    //         </div>;
-    //     } else if (mov.id >= 39) {
-    //         return <div className="Article">
-    //             <div className="header-list">
-    //                 <Link to={"/wiecej/10" + "/" + (Number(mov.id) - 1)}>&#8592; Poprzedni
-    //                     artykuł</Link>
-    //                 <Link to={"/10"}>Artykuły</Link>
-    //                 <Link to="/">Strona główna</Link>
-    //             </div>
-    //             <div className="header-container">{list}</div>
-    //         </div>;
-    //     }  else {return <div className="Article">
-    //         <div className="header-list">
-    //             <Link
-    //                 to={"/wiecej/10"  + "/" + (Number(mov.id) - 1)}>&#8592; Poprzedni
-    //                 artykuł</Link>
-    //             <Link to={"/10" }>Artykuły</Link>
-    //             <Link to="/">Strona główna</Link>
-    //             <Link to={"/wiecej/10" + "/" + (Number(mov.id) + 1)}>Następny
-    //                 artykuł &rarr;</Link>
-    //         </div>
-    //         <div className="header-container">{list}</div>
-    //     </div>;
-    //     }
+            
     }
 }
 
